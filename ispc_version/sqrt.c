e0beaa6947be635d47fcbd30b791eae3ea6e1356
Corentin <Group19@external2.orbit-lab.org>
2016-11-21 13:41:20 -0500

    added sequental sqrt to ispc version

--- ispc_version/sqrt.c
+++ ispc_version/sqrt.c
@@ -1,9 +1,12 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <time.h>
 #include "sqrt_ispc.h"
 
 using namespace ispc;
 
+const float epsilon = 0.0001f;
+const float divisor = 2f;
 const float MAX = 5;
 const float MIN = 0;
 /* Change this to whatever you'd like! */
@@ -11,20 +14,29 @@ const int NUM_ROOTS = 1024;
 
 int main()
 {
-	float x[NUM_ROOTS];
-	float ans[NUM_ROOTS];
+	clock_t begin, end;
+	double timeSpent;
+	float x[NUM_ROOTS], x_seq[NUM_ROOTS];
+	float ans[NUM_ROOTS], ans_seq[NUM_ROOTS];
 
 	int i;
 	
 	// initialize x with random floats within [MIN, MAX]
 	for (i = 0; i < NUM_ROOTS; i++)
 	{
-		x[i] = (float) rand()/(float)(RAND_MAX/MAX) + MIN;
+		float rand_num = (float) rand()/(float)(RAND_MAX/MAX) + MIN;
+		x[i] = rand_num;
+		x_seq[i] = rand_num;
 	}
 	
 	// call sqrt in parallel for all NUM_ROOTS numbers
+	begin = clock();
 	sqrt_ispc(NUM_ROOTS, x, ans);
+	end = clock();
+        timeSpent = (double)(end - begin) / CLOCKS_PER_SEC;
 	
+	printf("Time for ispc sqrt of %d elements: %f", NUM_ROOTS, timeSpent);
+
 	// print out if you want to verify
 	/*
 	for (i = 0 ; i < NUM_ROOTS; i++)
@@ -32,6 +44,27 @@ int main()
 		printf("%f: %f\n", x[i], ans[i]);
 	}
 	*/
+
+	// now do it sequentially
+	begin = clock();
+	sqrt_seq(x_seq, ans_seq);
+	end = clock();
+        timeSpent = (double)(end - begin) / CLOCKS_PER_SEC;
 	
+	printf("Time for sequential sqrt of %d elements: %f", NUM_ROOTS, timeSpent);
+
 	return 0;
+}
+
+void sqrt_seq(float x[], float ans[])
+{
+	int i;
+	for (i = 0; i < NUM_ROOTS; i++)
+        {
+                ans[i] = x[i];
+                while (epsilon * ans[i] < Math.abs(ans[i] - x[i]/ans[i]))
+                {
+                        ans[i] = (x[i]/ans[i] + ans[i])/divisor;
+                }
+        }
 }
