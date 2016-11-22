818de3a391733217e60842fc82b859b55e00bdc8
shreyashirday@gmail.com <shreyashirday@gmail.com>
2016-11-22 16:40:38 -0500

    final working

--- ispc_version/sqrt.cpp
+++ ispc_version/sqrt.cpp
@@ -13,7 +13,7 @@ const float divisor = 2.f;
 const float MAX = 5;
 const float MIN = 0;
 /* Change this to whatever you'd like! */
-const int NUM_ROOTS = 16777216;
+const int NUM_ROOTS = 65536;
 
 int main()
 {
@@ -33,6 +33,7 @@ int main()
 	int num_cores;
 	int num_threads;
 	// NO TASKS: 1-8 threads
+  printf("ispc\n");
 	for (num_threads = 1; num_threads <= 8; num_threads++)
 	{
 		begin = clock();
@@ -40,8 +41,7 @@ int main()
 		end = clock();
 		timeSpent = (double)(end - begin) / CLOCKS_PER_SEC;
 
-		printf("[sqrt ispc] [%d elements] [1 core | %d thread(s)]: [%f] cycles\n",
-			NUM_ROOTS,
+		printf("1 | %d thread(s) | %f cycles\n",
 			num_threads,
 			timeSpent);
 	}
@@ -56,8 +56,7 @@ int main()
 			end = clock();
 			timeSpent = (double)(end - begin) / CLOCKS_PER_SEC;
 
-			printf("[sqrt ispc] [%d elements] [%d cores | %d thread(s)]: [%f] cycles\n",
-				NUM_ROOTS,
+			printf("%d | %d thread(s) | %f cycles\n",
 				num_cores,
 				num_threads,
 				timeSpent);
@@ -65,6 +64,7 @@ int main()
 	}
 
 	// print out if you want to verify
+
 	/*
 	for (i = 0 ; i < NUM_ROOTS; i++)
 	{
@@ -77,12 +77,8 @@ int main()
 	sqrt_seq(x, ans);
 	end = clock();
         timeSpentSeq = (double)(end - begin) / CLOCKS_PER_SEC;
-
-	printf("[sqrt seq] [%d elements]: [%f] cycles\n", NUM_ROOTS, timeSpentSeq);
-
-	// free all allocated memory
-	free(x);
-	free(ans);
+  printf("sequential\n");
+	printf("%f cycles\n", timeSpentSeq);
 
 	return 0;
 }
