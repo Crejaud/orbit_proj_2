73ccfdd50d63992d39ed94c77240efd4a8796575
Ali <syed.naqvi@rutgers.edu>
2016-11-24 22:00:52 -0500

    ljksfd

--- ispc_version/sqrt.cpp
+++ ispc_version/sqrt.cpp
@@ -16,18 +16,17 @@ const float divisor = 2.f;
 const float MAX = 5;
 const float MIN = 0;
 /* Change this to whatever you'd like! */
-#define NUM_ROOTS 4194304;
 
 int main()
 {
 	clock_t begin, end;
 	double timeSpent, timeSpentIspc, timeSpentSeq;
-	float x[NUM_ROOTS];
-	float ans[NUM_ROOTS];
+	float x[4194304];
+	float ans[4194304];
 	int i;
 	printf("starting\n");
 	// initialize x with random floats within [MIN, MAX]
-	for (i = 0; i < NUM_ROOTS; i++)
+	for (i = 0; i < 4194304; i++)
 	{
 		x[i] = (float) rand()/(float)(RAND_MAX/MAX) + MIN;
 	}
@@ -40,7 +39,7 @@ int main()
 	for (num_threads = 1; num_threads <= 8; num_threads++)
 	{
 		begin = clock();
-		sqrt_ispc(NUM_ROOTS, x, ans, num_threads);
+		sqrt_ispc(4194304, x, ans, num_threads);
 		end = clock();
 		timeSpent = (double)(end - begin) / CLOCKS_PER_SEC;
 
@@ -56,7 +55,7 @@ printf("test1\n");
 		for (num_threads = 1; num_threads <= 8; num_threads++)
 		{
 			begin = clock();
-			sqrt_ispc_tasks(NUM_ROOTS, x, ans, num_cores, num_threads);printf("test2\n");
+			sqrt_ispc_tasks(4194304, x, ans, num_cores, num_threads);printf("test2\n");
 			end = clock();printf("test3\n");
 			timeSpent = (double)(end - begin) / CLOCKS_PER_SEC;
 printf("test4\n");
@@ -98,7 +97,7 @@ printf("test4\n");
 void sqrt_seq(float x[], float ans[])
 {
 	int i;
-	for (i = 0; i < NUM_ROOTS; i++)
+	for (i = 0; i < 4194304; i++)
         {
                 ans[i] = x[i];
                 while (epsilon * ans[i] < fabsf(ans[i] - x[i]/ans[i]))
@@ -111,7 +110,7 @@ void sqrt_seq(float x[], float ans[])
 void sqrt_avx_loop(float x[], float ans[])
 {
 	int i;
-	for (i = 0; i < NUM_ROOTS/8; i++)
+	for (i = 0; i < 4194304/8; i++)
 	{
 		__m256 x_avx = _mm256_set_ps(x[8*i], x[8*i+1], x[8*i+2], x[8*i+3], x[8*i+4], x[8*i+5],x[8*i+6], x[8*i+7]);
 		sqrt_avx(x_avx);
