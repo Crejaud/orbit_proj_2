3bafac049dc1197241ecee308236e8d84f3c2229
thulfeqar <anaqvi@live.com>
2016-11-25 00:11:40 -0500

    fsdkl

--- ispc_version/sqrt.cpp
+++ ispc_version/sqrt.cpp
@@ -10,32 +10,27 @@ using namespace ispc;
 void sqrt_seq(float x[], float ans[]);
 void sqrt_avx_loop(float x[], float ans[]);
 void sqrt_avx(__m256 x_avx);
-
+int testtest=0;
 const float epsilon = 0.0001f;
 const float divisor = 2.f;
 const float MAX = 5;
 const float MIN = 0;
+
 /* Change this to whatever you'd like! */
-const long long NUM_ROOTS=2097152;
+const long long NUM_ROOTS=8;
 int main()
 {
-	printf("1\n");
 	clock_t begin, end;
-	printf("2\n");
 	double timeSpent, timeSpentIspc, timeSpentSeq;
-	printf("x\n");
 	float x[NUM_ROOTS];
-	printf("Ans\n");
 	float ans[NUM_ROOTS];
 	unsigned long long i;
-	printf("try here\n");
 	// initialize x with random floats within [MIN, MAX]
 	for (i = 0; i < NUM_ROOTS; i++)
 	{
-		printf("index %d\n", i);
-		x[i] = (float) rand()/(float)(RAND_MAX/MAX) + MIN;
+		x[i] = (float) (rand()%6);
 	}
-
+	printf("initial %f\n", x[0]);
 	// CALLING SQRT IN ISPC
 	int num_cores;
 	int num_threads;
@@ -44,26 +39,25 @@ int main()
 	for (num_threads = 1; num_threads <= 8; num_threads++)
 	{
 		begin = clock();
-		sqrt_ispc(NUM_ROOTS, x, ans, num_threads);
-		end = clock();
+	printf("first try first %f and %f\n", x[0],x[1]);
+	sqrt_ispc(NUM_ROOTS, x, ans, num_threads);
+		printf("first try second %f\n", x[0]);end = clock();
 		timeSpent = (double)(end - begin) / CLOCKS_PER_SEC;
 
 		printf("1 | %d thread(s) | %f cycles\n",
 			num_threads,
 			timeSpent);
-printf("test0\n");
 	}
-printf("test1\n");
+	printf("middle %f\n", x[0]);
 	// TASKS: 2-4 cores & 1-8 threads
 	for (num_cores = 2; num_cores <= 4; num_cores++)
 	{
 		for (num_threads = 1; num_threads <= 8; num_threads++)
 		{
 			begin = clock();
-			sqrt_ispc_tasks(NUM_ROOTS, x, ans, num_cores, num_threads);printf("test2\n");
-			end = clock();printf("test3\n");
+			sqrt_ispc_tasks(NUM_ROOTS, x, ans, num_cores, num_threads);
+			end = clock();
 			timeSpent = (double)(end - begin) / CLOCKS_PER_SEC;
-printf("test4\n");
 			printf("%d | %d thread(s) | %f cycles\n",
 				num_cores,
 				num_threads,
@@ -82,14 +76,15 @@ printf("test4\n");
 
 	// now do it avx
 	begin = clock();
+	printf("moved x %f\n", x[0]);
 	sqrt_avx_loop(x, ans);
 	end = clock();
         timeSpentSeq = (double)(end - begin) / CLOCKS_PER_SEC;
-  	printf("avx\n");
+  	printf("avx, %d\n", testtest);
 	printf("%f cycles\n", timeSpentSeq);
 
 	// now do it sequentially
-	begin = clock();
+	begin = clock();	printf("seqential x %d\n", x[0]);
 	sqrt_seq(x, ans);
 	end = clock();
         timeSpentSeq = (double)(end - begin) / CLOCKS_PER_SEC;
@@ -114,10 +109,12 @@ void sqrt_seq(float x[], float ans[])
 
 void sqrt_avx_loop(float x[], float ans[])
 {
+	printf("inner %f\n", x[0]);
 	unsigned long long i;
 	for (i = 0; i < NUM_ROOTS/8; i++)
 	{
 		__m256 x_avx = _mm256_set_ps(x[8*i], x[8*i+1], x[8*i+2], x[8*i+3], x[8*i+4], x[8*i+5],x[8*i+6], x[8*i+7]);
+		//printf("initialize %d %d  %d\n",i,  x[8*i], x_avx[0]);
 		sqrt_avx(x_avx);
 	}
 }
@@ -129,7 +126,7 @@ void sqrt_avx(__m256 x_avx)
 	__m256 diff = _mm256_sub_ps(x_avx, ans_avx);
 
 	bool diffmag = false;
-
+	//printf("unrooted %d\n", x_avx[0]);
 	unsigned long long i;
 	for (i = 0; i < 8; i++)
 	{
@@ -155,4 +152,5 @@ void sqrt_avx(__m256 x_avx)
 			}
 		}
 	}
+	//printf("rooted %d\n", ans_avx[0]);
 }
